2a362a34f2bdf77cda26cf8f029c6441869f20c6
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-06-26 17:51:06 -0300

    Atividade 2 de fixação JS - 26/06/2023 Tquarta
    Criar duas variáveis usando a palavra-chave var.
    
    2)      Criar duas variáveis usando a palavra-chave let.
    
    3)      Criar duas constantes usando a palavra-chave const.

---
+++ Atividade 2 de fixação Java Script - Turma 1.js
@@ -0,0 +1,18 @@
+var nome_aluno = "Ana Carolina";
+console.log(nome_aluno);
+
+nome_aluno = "Felipe";
+console.log(nome_aluno);
+
+let idade_aluno = 16;
+console.log(idade_aluno);
+
+idade_aluno = 13;
+console.log(idade_aluno);
+
+const PI = 3.14159;
+console.log(PI);
+
+const matricula_aluno = 157908465;
+console.log(matricula_aluno);
+
