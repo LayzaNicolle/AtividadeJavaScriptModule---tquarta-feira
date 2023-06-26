dfefe861a76f6cd18a625f189dc0283cbdac5d35
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-06-26 17:30:50 -0300

    Atividade 26/06/2023 - Tquarta-feira
    Atividade de classe - Tipos de dados Java Script

---
+++ Tipos de dados.js
@@ -0,0 +1,70 @@
+//tipos de dados: Number
+var numero_inteiro = 20;
+console.log(numero_inteiro);
+
+var idade = 19;
+console.log(idade);
+
+//tipos de dados: string com aspas duplas
+var nome_aluno = "Luiza";
+console.log(nome_aluno);
+
+//tipos de dados: string com aspas simples
+var nome_peca = 'blusa';
+console.log(nome_peca);
+
+//tipos de dados: booolean
+
+var verdade = true;
+var mentira = false;
+
+console.log("Você teve aula hoje?");
+console.log(verdade);
+
+console.log("Você precisou fazer prova?");
+console.log(mentira);
+
+//tipos de dados: array
+
+var material_escolar = ["caneta", "lápis", "caderno"];
+console.log(material_escolar)
+
+var sucos = ["laranja", "graviola", "maracujá"];
+console.log(sucos)
+
+//tipos de dados: object
+
+var roupa = {modelo: 'short', material: 'jeans', tamanho: 34};
+console.log(roupa);
+
+console.log(roupa.modelo);
+
+console.log(roupa.material);
+
+var celular = {marca: 'apple', modelo: 'iphone 12', cor: 'preto'}
+console.log(celular);
+
+console.log(celular.marca);
+
+console.log(celular.modelo);
+
+//tipos de dados: Undefined 
+
+var valor_nao_definido;
+
+console.log("idade = " + valor_nao_definido);
+
+var valor_nao_definido;
+
+console.log("CPF = " + valor_nao_definido);
+
+//tipos de dados: Null
+
+var valor_vazio = null;
+
+console.log("Código do cliente = " + valor_vazio);
+
+var valor_vazio = null;
+
+console.log("Cadastro na loja = " + valor_vazio);
+
