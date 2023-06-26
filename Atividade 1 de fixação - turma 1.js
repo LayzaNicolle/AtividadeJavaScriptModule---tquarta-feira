6cf632764360b7e45b72cfc29a4483efff9dd832
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-06-26 17:42:35 -0300

    Atividade 1 de fixação Java Script - turma 1
    1) Criar variáveis com tipos de dados:
    a) Number
    
    b) String
    
    c) Boolean
    
    d) Array
    
    e) Object
    
    f) Undefined
    
    g) Null

---
+++ Atividade 1 de fixação - turma 1.js
@@ -0,0 +1,45 @@
+//variáveis com dados do tipo Number
+var numero_chamada = 12;
+console.log(numero_chamada);
+
+//variáveis com dados do tipo string 
+var nome_paciente= "André";
+console.log(nome_paciente);
+
+//variáveis com dados do tipo booolean
+
+var verdade = true;
+var mentira = false;
+
+console.log("Você foi à consulta hoje?");
+console.log(verdade);
+
+console.log("Você precisou realizar exame de sangue?");
+console.log(mentira);
+
+//variáveis com dados do tipo array
+
+var material_reforma = ["Cimento", "Tijolo", "Areia"];
+console.log(material_reforma)
+
+//variáveis com dados do tipo object
+
+var telivisao = {marca: 'samsumg', cor: 'preta', modelo: 'smart 43'};
+console.log(televisao);
+
+console.log(televisao.marca);
+
+console.log(televisao.modelo);
+
+//tipos de dados: Undefined 
+
+var valor_nao_definido;
+
+console.log("CPF = " + valor_nao_definido);
+
+//tipos de dados: Null
+
+var valor_vazio = null;
+
+console.log("Senha = " + valor_vazio);
+
